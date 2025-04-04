8862da9159abcd17b1d40486acff2d1a427776e7
Liby <libypa123@gmail.com>
2025-04-04 13:20:31 +0100

    Variables and identifiers

---
+++ Variable_Identifier.java
@@ -0,0 +1,17 @@
+
+public class Variable_Identifier {
+    //variables are containers for storing data values.
+    //identifiers are the unique names given to variables.
+    //program that stores different data about a student
+    public static void main(String[] args) {
+        String name = "Paru";
+        int studentID = 25;
+        float fee = 75.3f;
+        char grade = 'A';
+        boolean studentPass = true;
+        System.out.println("Student name: " + name);
+        System.out.println("Student id: " + studentID);
+        System.out.println("Student fee: " + fee);
+        System.out.println("Student grade: " + grade);
+    }
+}

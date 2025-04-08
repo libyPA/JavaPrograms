c8f9d38511547bed8b7749d71c1021e55fdade34
Liby <libypa123@gmail.com>
2025-04-08 22:07:09 +0100

    executed new way of switch loop

--- SwitchLoop.java
+++ SwitchLoop.java
@@ -1,5 +1,19 @@
-public class SwitchLoop {
+public class SwitchLoop {//Java 14 onwards
     public static void main(String[] args) {
-
+    int day =1;
+    String dayName = switch(day){
+        case 1 -> "Monday";
+        case 2-> "Tuesday";
+        default -> "Wrong Input";
+        };
+        System.out.println(dayName);
+        System.out.println(check(2));
+    }
+    public static String check(int day){
+        return switch(day){
+            case 1 -> "Monday";
+            case 2-> "Tuesday";
+            default -> "Wrong Input";
+        };
     }
 }
